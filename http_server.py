fba5945300b8a8b5f4860ac831645411e3de2715
Sebastien Fortas <sebastien@fortas.org>
2017-06-21 16:43:36 -0400

    basic function complete

--- http_server.py
+++ http_server.py
@@ -2,6 +2,7 @@ import os
 import json
 import bottle
 import datetime
+import functools32
 
 static_dir = os.path.join(os.path.dirname(__file__), 'static')
 spycam_dir = os.path.join(os.path.dirname(__file__), 'pix')
@@ -36,11 +37,24 @@ def server_static(filename):
 def get_original(filename):
     return bottle.static_file(filename, root=spycam_dir)
 
+@functools32.lru_cache(maxsize=1024)
+def create_thumbnail(filename):
+    from PIL import Image
+    from StringIO import StringIO
+    print 'Creating thumbnail for {}'.format(filename)
+    path = os.path.join(spycam_dir, filename)
+    assert os.path.exists(path), 'No such file: {}'.format(path)
+    io = StringIO()
+    im = Image.open(path)
+    im.thumbnail((128,128))
+    im.save(io, 'JPEG')
+    return io.getvalue()
+
 
 @bottle.route('/thumb/<filename>')
-def create_thumbnail(filename):
-    # TODO: thumbnail creation magic
-    return None
+def get_thumbnail(filename):
+    bottle.response.content_type = 'image/jpeg'
+    return create_thumbnail(filename)
 
 
 if __name__ == '__main__':
