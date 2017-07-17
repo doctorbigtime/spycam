d9f56db9b9e0b0834db048de6bbcdeab6dbecd77
Sebastien Fortas <sebastien@fortas.org>
2017-07-17 12:22:20 -0400

    working version of server

--- http_server.py
+++ http_server.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 import bottle
 import datetime
@@ -6,17 +7,39 @@ import functools32
 
 static_dir = os.path.join(os.path.dirname(__file__), 'static')
 spycam_dir = os.path.join(os.path.dirname(__file__), 'pix')
+if len(sys.argv) > 1 and os.path.isdir(sys.argv[1]):
+    spycam_dir = sys.argv[1]
 
 
 @bottle.route('/')
-def index():
-    return bottle.static_file('index.html', root='.')
+def home():
+    subdirs = [x for x in os.listdir(spycam_dir) if os.path.isdir(os.path.join(spycam_dir, x))]
+    return bottle.template('index', curr_dir='', subdirs=subdirs)
 
 
-@bottle.route('/dates.json')
-def generate_dates():
+@functools32.lru_cache(maxsize=128)
+def get_subdirs(path):
+    print 'path: "{}"'.format(path)
+    d = os.path.join(spycam_dir, path)
+    print 'd: "{}"'.format(d)
+    return sorted([x for x in os.listdir(d) if os.path.isdir(os.path.join(d, x))])
+
+
+@bottle.route('/index/<path:path>')
+def index(path):
+    subdirs = get_subdirs(path)
+    if subdirs:
+        return bottle.template('index', curr_dir=path, subdirs=subdirs)
+    else:
+        return bottle.template('timeline', curr_dir=path)
+
+
+@bottle.route('/<path:path>/dates.json')
+@functools32.lru_cache(maxsize=1024)
+def generate_dates(path):
+    print 'generate dates - path: "{}"'.format(path)
     dates = []
-    for file in os.listdir(spycam_dir):
+    for file in os.listdir(os.path.join(spycam_dir, path)):
         try:
             date_split = file.split('-')
             dt = datetime.datetime.strptime(date_split[0], '%Y%m%d%H%M%S')
@@ -28,21 +51,23 @@ def generate_dates():
             continue
     return json.dumps({'dates':sorted(dates)}, indent=4)
 
+
 @bottle.route('/static/<filename>')
 def server_static(filename):
     return bottle.static_file(filename, root=static_dir)
 
 
-@bottle.route('/picture/<filename>')
-def get_original(filename):
-    return bottle.static_file(filename, root=spycam_dir)
+@bottle.route('/picture/<path:path>/<filename>')
+def get_original(path, filename):
+    return bottle.static_file(os.path.join(path, filename), root=spycam_dir)
+
 
 @functools32.lru_cache(maxsize=1024)
-def create_thumbnail(filename):
+def create_thumbnail(path, filename):
     from PIL import Image
     from StringIO import StringIO
     print 'Creating thumbnail for {}'.format(filename)
-    path = os.path.join(spycam_dir, filename)
+    path = os.path.join(spycam_dir, path, filename)
     assert os.path.exists(path), 'No such file: {}'.format(path)
     io = StringIO()
     im = Image.open(path)
@@ -51,10 +76,10 @@ def create_thumbnail(filename):
     return io.getvalue()
 
 
-@bottle.route('/thumb/<filename>')
-def get_thumbnail(filename):
+@bottle.route('/thumb/<path:path>/<filename>')
+def get_thumbnail(path, filename):
     bottle.response.content_type = 'image/jpeg'
-    return create_thumbnail(filename)
+    return create_thumbnail(path, filename)
 
 
 if __name__ == '__main__':
